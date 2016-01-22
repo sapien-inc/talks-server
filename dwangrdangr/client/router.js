d855184003c8a1483fb76412eda929411ba63cb1
Harrison Wang <harrison.wang@appboy.com>
2016-01-21 22:23:00 -0500

    first additions

--- dwangrdangr/client/router.js
+++ dwangrdangr/client/router.js
@@ -6,10 +6,6 @@ Router.map(function(){
   this.route('about',{path:'/about'})
   this.route('home',{path:'/'})
   this.route('mainpage',{path:'/mainpage'})
-  this.route('results',{path:'/results/:search?',
-      data: function(){
-        return { search : Router.current().params.search}
-      }
-  })
+  this.route('results',{path:'/results/:search?'})
 })
 
